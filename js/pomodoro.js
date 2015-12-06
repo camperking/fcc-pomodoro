5717917e40764888d0a02ba7ef756eeee5002d08
camperking <camperking@gmail.com>
2015-12-07 00:04:50 +0100

    working
    working commit

---
+++ js/pomodoro.js
@@ -0,0 +1,91 @@
+$(document).ready(function(){
+    var timechange = true;
+
+    $("#slider-range").slider({
+        range: true,
+        min: 1,
+        max: 120,
+        values: [5, 25],
+        slide: function( event, ui ) {
+            $("#break").val(ui.values[0]);
+            $("#session").val(ui.values[1]);
+            if (running === false) {$("#time-remaining").text(secondsToHms(ui.values[1] * 60));}
+            timechange = true;
+        }
+    });
+    $("#break").val($("#slider-range").slider("values", 0));
+    $("#session").val($("#slider-range").slider("values", 1));
+    $("#time-remaining").text(secondsToHms($("#slider-range").slider("values", 1)*60));
+
+    var timer;
+    var running = false;
+    var timeremaining;
+    //var breaktime = 0;
+    //var sessiontime = 0;
+    $(".clock").on("click", function () {
+        breaktime = $("#slider-range").slider("values", 0) * 1000 * 60;
+        sessiontime = $("#slider-range").slider("values", 1) * 1000 * 60;
+
+        if (running === false && timechange === true) {
+            $("#state").text("Session");
+            time_remaining = sessiontime;
+            running = true;
+            timer = window.setInterval(function() {starttimer();}, 1000);
+        } else if (running === false && timechange === false) {
+            if (session === true) {
+                        $("#state").text("Session");
+            } else {
+                        $("#state").text("Break");
+            }
+            running = true;
+            timer = window.setInterval(function() {starttimer();}, 1000);
+        } else if (running === true && timechange == true) {
+            window.clearInterval(timer);
+            $("#state").text("Start");
+            $("#time-remaining").text(secondsToHms($("#slider-range").slider("values", 1)*60));
+            time_remaining = sessiontime;
+            session = true;
+            running = false;
+        } else {
+            window.clearInterval(timer);
+            $("#state").text("Start");
+            running = false;
+        }
+
+        timechange = false;
+    });
+});
+
+
+function starttimer () {
+    time_remaining -= 1000;
+    $("#time-remaining").text(secondsToHms(time_remaining / 1000));
+
+    if (session) {
+        $(".filler").height(100 - time_remaining/sessiontime*100 + "%");
+    } else {
+        $(".filler").height(100 - time_remaining/breaktime*100 + "%");
+    }
+
+    if (time_remaining <= 0 && session) {
+        time_remaining = breaktime;
+        $("#state").text("Break");
+        session = false;
+    }
+
+    if (time_remaining <= 0 && !session) {
+        time_remaining = sessiontime;
+        $("#state").text("Session");
+        session = true;
+    }
+}
+
+function secondsToHms(d) {
+    d = Number(d);
+    var h = Math.floor(d / 3600);
+    var m = Math.floor(d % 3600 / 60);
+    var s = Math.floor(d % 3600 % 60);
+    return (
+        (h > 0 ? h + ":" + (m < 10 ? "0" : "") : "") + m + ":" + (s < 10 ? "0" : "") + s
+    );
+}
